d16233b3bbf0167f958d1f5451560649d43c5230
abdul waris <warismuthuparamba@gmail.com>
2024-05-23 15:05:31 +0530

    created project

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { SignupComponet } from "./components/signup/signup.component";
+
+@NgModule({
+    declarations:[
+        SignupComponet
+    ],
+    imports:[
+
+    ],
+    providers:[
+
+    ]
+})
+
+export class SignupModule{}
